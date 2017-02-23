70a3904b5b60235e5ac34acded67a244382246c0
Alan el <mail@mjniuz.com>
2017-02-23 15:01:04 +0700

    add msg

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -19,16 +19,56 @@ class GetMessageService
     
     public function replySend($formData)
     {
-        $replyToken = $formData['events']['0']['replyToken'];
-        
+        $ev = $formData['events']['0'];
+
+        $replyToken = $ev['replyToken'];
         $this->client = new CurlHTTPClient(env('LINE_BOT_ACCESS_TOKEN'));
         $this->bot = new LINEBot($this->client, ['channelSecret' => env('LINE_BOT_SECRET')]);
-        
-        $response = $this->bot->replyText($replyToken, 'hello!');
+
+        $msgResponse = $this->getMsg($ev);
+        $response = $this->bot->replyText($replyToken, $msgResponse);
         
         if ($response->isSucceeded()) {
             logger("reply success!!");
             return;
         }
+    }
+
+    private function getMsg($ev = []){
+        $msgType = !empty($ev['message']) ? $ev['message']['type'] : false;;
+        if($msgType == 'text'){
+            $msgUser = $ev['message'];
+
+            return $this->collectMsg($msgUser);
+        }
+
+        return '';
+    }
+
+    private function collectMsg($msgUser = ''){
+        switch ($msgUser){
+            case 'help':
+                $response = $this->help();
+                break;
+            case 'maen meme':
+                $response = $this->meme();
+                break;
+            default:
+                $response = $this->help();
+        }
+
+        return $response;
+    }
+
+    private function help(){
+        $msg = 'Buat lo yang pengen bikin meme, silahkan ketik: \n
+                *maen Meme*';
+
+        return $msg;
+    }
+
+    private function meme(){
+        $msg = 'Tulis kata untuk menaruh gambar diatas';
+        return $msg;
     }
 }
