0b360b07f8bce2419bbac7419f432ff0eb36a995
Alan el <mail@mjniuz.com>
2017-02-23 22:53:46 +0700

    disable image test

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -172,7 +172,7 @@ class GetMessageService
         $response = $this->bot->getMessageContent($msgID);
 
         if ($response->isSucceeded()) {
-            return $response->isSucceeded().'ok'; //$response->getRawBody() || json_encode([$response,$msgID]);
+            return $response->getHeaders().'ok'; //$response->getRawBody() || json_encode([$response,$msgID]);
         } else {
             error_log($response->getHTTPStatus() . ' ' . $response->getRawBody());
         }
