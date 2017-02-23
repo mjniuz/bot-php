f10ab39fcb0bc19eb7c6fbf63ae3b33abf0a41d6
Alan el <mail@mjniuz.com>
2017-02-23 15:11:21 +0700

    add msg json

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -25,8 +25,8 @@ class GetMessageService
         $this->client = new CurlHTTPClient(env('LINE_BOT_ACCESS_TOKEN'));
         $this->bot = new LINEBot($this->client, ['channelSecret' => env('LINE_BOT_SECRET')]);
 
-        //$msgResponse = $this->getMsg($ev);
-        $response = $this->bot->replyText($replyToken,  json_encode($ev));
+        $msgResponse = $this->getMsg($ev);
+        $response = $this->bot->replyText($replyToken,  $msgResponse);
         
         if ($response->isSucceeded()) {
             logger("reply success!!");
@@ -37,9 +37,9 @@ class GetMessageService
     private function getMsg($ev = []){
         $msgType = !empty($ev['message']) ? $ev['message']['type'] : false;
         if($msgType == 'text'){
-            $msgUser = $ev['message'];
+            $msgUser = $ev['message']['text'];
 
-            return $msgUser;
+            return $this->collectMsg($msgUser);
         }
 
         return 'no msg';
